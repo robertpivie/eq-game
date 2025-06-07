85a8d82d45790934b708a2bc6733a54883c7184c
Robert Ivie <robertivie@Roberts-MacBook-Pro.local>
2025-06-07 12:54:55 -0600

    added level 1 and 2

---
+++ src/app/lv1/page.tsx
@@ -0,0 +1,10 @@
+export default function Lv1() {
+    return (
+        <section>
+            <h1>
+                Level 1
+            </h1>
+            <canvas />
+        </section>
+    );
+};
---
+++ src/app/lv2/page.tsx
@@ -0,0 +1,10 @@
+export default function Lv2() {
+    return (
+        <section>
+            <h1>
+                Level 2
+            </h1>
+            <canvas />
+        </section>
+    );
+};
